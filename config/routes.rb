b1672da145cacdd660fbfa369cd5abe8390618ad
Tara MacKinnon <taramackinnon4@gmail.com>
2023-05-04 23:30:03 +0000

    changed about controller to pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
     resources :categories, only: [:index, :new, :create]
   end
   
-  get 'about' => 'about#index'
+  get '/about', to: 'pages#index'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
