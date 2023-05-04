06a8de6474d7624d07492be4ac6525e66a19a236
Tara MacKinnon <taramackinnon4@gmail.com>
2023-05-04 04:34:12 +0000

    update admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
   
   def show
+    @number_of_products = Product.count
+    @number_of_categories = Category.count
   end
 end
