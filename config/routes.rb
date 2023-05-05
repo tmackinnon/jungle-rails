ad154af4c59db562d2b710be5e3922f21c51b96c
Tara MacKinnon <taramackinnon4@gmail.com>
2023-05-05 15:44:56 +0000

    add routes, controller and views for user auth

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,13 @@ Rails.application.routes.draw do
   end
   
   get '/about', to: 'pages#index'
+
+  get '/signup', to: 'users#new'
+  post '/users', to: 'users#create'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
