4062e4ee8c2577c64adf795ac41053a649bf2564
Tara MacKinnon <taramackinnon4@gmail.com>
2023-05-08 23:47:59 +0000

    add user validation specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :email, presence: true
-  validates :password, presence: true
+  validates :password, presence: true, confirmation: {case_sensitive: false }
   validates :password_confirmation, presence: true
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,18 +3,31 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   
   describe 'Validations' do
-    # before do
-    #   @user = User.new(name: "Tara", email: "t@t.com", password: "1234", password_confirmation: "1234")
-    # end
+    before do
+      @user = User.new(name: "Tara", email: "t@t.com", password: "password", password_confirmation: "password")
+    end
 
-    # it "is valid with valid attributes" do
-    #   expect(@user).to be_valid
-    # end
+    it "is valid with valid attributes" do
+      expect(@user).to be_valid
+    end
 
-    # it "must be created with a password and password_confirmation fields" do
-    #   expect(@user.password).to eq(@user.password_confirmation)
-    # end
+    it "is not valid without password" do
+      @user.password = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to_not be_empty
+    end
 
+    it "is not valid without password_confirmation" do
+      @user.password_confirmation = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to_not be_empty
+    end
+
+    it "is not valid when password and password_confirmation don't match" do
+      @user.password = "pAssWord"
+      @user.valid?
+      expect(@user.errors.full_messages).to_not be_empty
+    end
 
 
 
