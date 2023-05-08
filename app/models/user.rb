ae4bbe9b787414605ce72964c1a3102215ce5255
Tara MacKinnon <taramackinnon4@gmail.com>
2023-05-08 23:16:12 +0000

    start user tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  
+  describe 'Validations' do
+    # before do
+    #   @user = User.new(name: "Tara", email: "t@t.com", password: "1234", password_confirmation: "1234")
+    # end
+
+    # it "is valid with valid attributes" do
+    #   expect(@user).to be_valid
+    # end
+
+    # it "must be created with a password and password_confirmation fields" do
+    #   expect(@user.password).to eq(@user.password_confirmation)
+    # end
+
+
+
+
+  end
+
+end
